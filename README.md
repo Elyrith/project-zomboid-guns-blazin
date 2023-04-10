c3feac9481176c08904996f2089ea73e86e95f3c
Ryan <ryan@loudfoot.ca>
2023-04-09 23:15:47 -0400

    Catch more of the desired weapons in the filters

--- README.md
+++ README.md
@@ -8,20 +8,19 @@ Changes:
 1. For any item with DisplayCategory of "Ammo"
  1. "Weight" = 0
 2. For any item that has DisplayCategory of "Weapon":
- 1. "ConditionLowerChanceOneIn" = 9999999
- 2. "JamGunChance" = 9999999
-3. For any item with "Shotgun" in the name:
+ 1. "ConditionLowerChanceOneIn" = 999999
+ 2. "JamGunChance" = 0
+3. Any "Shotgun": (based on SwingSound)
  1. "MaxHitCount" = 10 (only matters if you turn multi-hit on in the sandbox settings)
-4. For any item with "Katana" or "Machete" (or "Sword" for other mods) in the name:
- 2. "MaxRange" = "MaxRange" * 5
- 3. "MaxHitCount" = 10 (only matters if you turn multi-hit on in the sandbox settings)
- 4. "MinAngle" = 0 (hit nearest visibile enemy/enemies)
+4. Any "Katana" or "Machete" (or "Sword" for other mods): (based on SwingSound)
+ 1. "MaxRange" = "MaxRange" * 5
+ 2. "MaxHitCount" = 10 (only matters if you turn multi-hit on in the sandbox settings)
+ 3. "MinAngle" = 0 (hit nearest visibile enemy/enemies)
 
 Known Issues:
-1. For some reason, not every weapon has a DisplayCategory of "Weapon". Some of them are "Cooking" (like Baking Pan). I'm working on this.
-2. The katana and machete MaxRange change only work on items that are created after you activate the mod.
+1. Since a lot of the changes are based on the SwingSound, this may not work on a weapon from a mod if it doesn't match the LUA find criteria.
 
-It should work with most mods that match the criteria above.
+It should work with most mods.
 
 I made this just so a bunch of friends and I could load up with weapons, set the population super-high, and fight our way to victory/failure.
 
--- contents/mods/guns-blazin/media/LUA/shared/guns_blazin.lua
+++ contents/mods/guns-blazin/media/LUA/shared/guns_blazin.lua
@@ -1,33 +1,38 @@
+function LongBladeChanges(item)
+    local maxRange = item:getMaxRange() * 5
+    item:DoParam("MaxRange = "..maxRange)
+    item:DoParam("MaxHitCount = 10")
+    item:DoParam("MinAngle = 0")
+end
+
 function GunsBlazin()
     local items = getAllItems()
     for i = 0, items:size() -1 do
+
         local item = items:get(i)
 
+        -- Weapons don't break and guns don't jam
+        item:DoParam("ConditionLowerChanceOneIn = 999999")
+        item:DoParam("JamGunChance = 0")
+    
         if item:getDisplayCategory() == "Ammo" then
             item:DoParam("Weight = 0")
-        elseif item:getDisplayCategory() == "Weapon" then
-            item:DoParam("ConditionLowerChanceOneIn = 100000")
-            item:DoParam("JamGunChance = 0")
         end
-        if string.match(item:getName(), "Shotgun") then
+        
+        -- I have had lots of issues using Type and Name. SwingSound seems to work for now.
+        -- This is for Shotguns
+        swingSound = string.lower(item:getSwingSound())
+        if string.find(swingSound, "shot") then
             item:DoParam("MaxHitCount = 10")
         end
 
-        if string.match(item:getName(), "Katana") then
-            local maxRange = item:getMaxRange() * 5
-            item:DoParam("MaxRange = "..maxRange)
-            item:DoParam("MaxHitCount = 10")
-            item:DoParam("MinAngle = 0")
-        elseif string.match(item:getName(), "Machete") then
-            local maxRange = item:getMaxRange() * 5
-            item:DoParam("MaxRange = "..maxRange)
-            item:DoParam("MaxHitCount = 10")
-            item:DoParam("MinAngle = 0")
-        elseif string.match(item:getName(), "Sword") then
-            local maxRange = item:getMaxRange() * 5
-            item:DoParam("MaxRange = "..maxRange)
-            item:DoParam("MaxHitCount = 10")
-            item:DoParam("MinAngle = 0")
+        -- I can't figure out how to just select Long Blade weapons, so this will do for now
+        if string.find(swingSound, "katana") then
+            LongBladeChanges(item)
+        elseif string.find(swingSound, "machete") then
+            LongBladeChanges(item)
+        elseif string.find(swingSound, "axe") then
+            LongBladeChanges(item)
         end
 
     end
